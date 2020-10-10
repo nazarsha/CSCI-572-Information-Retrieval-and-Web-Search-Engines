4fcce86b14e65662b45d3febe4f0410364281707
nazarsha <46286272+nazarsha@users.noreply.github.com>
2020-10-10 16:50:21 -0700

    Update Controller.java

--- HW2 Web Crawling/src/Controller.java
+++ HW2 Web Crawling/src/Controller.java
@@ -10,51 +10,11 @@ import edu.uci.ics.crawler4j.robotstxt.RobotstxtServer;
 
 public class Controller {
 
-	public static void main(String[] args) throws Exception {
-		// TODO Auto-generated method stub
-//		String crawlStorageFolder = "E:/programs/eclipse-jee-neon-3-win32-x86_64/workspace/CSCI572/HW2/data/crawl";
-//		 int numberOfCrawlers = 1;
-//		 CrawlConfig config = new CrawlConfig();
-//		 config.setCrawlStorageFolder(crawlStorageFolder);
-//		 int maxDepthOfCrawling = 16;
-//		 //int maxPagesToFetch = 20000;
-//		 int maxPagesToFetch = 1;
-//		 int politenessDelay = 2500;
-//		 String userAgentString = "Sol";
-//		 
-//
-//		 config.setMaxDepthOfCrawling(maxDepthOfCrawling);
-//		 config.setMaxPagesToFetch(maxPagesToFetch);
-//		 config.setPolitenessDelay(politenessDelay);
-//		 config.setUserAgentString(userAgentString);
-//		 
-//		 
-//		 
-//		 /*
-//		 * Instantiate the controller for this crawl.
-//		 */
-//		 PageFetcher pageFetcher = new PageFetcher(config);
-//		 RobotstxtConfig robotstxtConfig = new RobotstxtConfig();
-//		 RobotstxtServer robotstxtServer = new RobotstxtServer(robotstxtConfig, pageFetcher);
-//		 CrawlController controller = new CrawlController(config, pageFetcher, robotstxtServer);
-//		 /*
-//		 * For each crawl, you need to add some seed urls. These are the first
-//		 * URLs that are fetched and then the crawler starts following links
-//		 * which are found in these pages
-//		 */
-//		 controller.addSeed("http://www.viterbi.usc.edu/");
-//		
-//		 /*
-//		 * Start the crawl. This is a blocking operation, meaning that your code
-//		 * will reach the line after this only when crawling is finished.
-//		 */
-//		 controller.start(MyCrawler.class, numberOfCrawlers);
-		 
-		 
+	public static void main(String[] args) throws Exception {	 
 		 
 			int threads = 1;
 			int maxDepthOfCrawling = 16;
-			String folder = "E:/programs/eclipse-jee-neon-3-win32-x86_64/workspace/CSCI572/HW2/data/crawl";
+			String folder = "~/CSCI572/HW2/data/crawl";
 			int maxPagesToFetch = 20000;
 			CrawlConfig conf = new CrawlConfig();
 			int politenessDelay = 2500;
